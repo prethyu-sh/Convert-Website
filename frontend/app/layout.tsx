9774a5069686a69fdb9cfc60ee858728cc8be70a
prethyu-sh <prethyushbabu121@gmail.com>
2026-04-22 11:21:23 +0530

    Google search console verification
    Updated for the verification process of Google Search Console

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <meta name="google-site-verification" content="elsD1zwKAbGVOg55ZUAWgrtEzawaZo44UQrhoBnQNZU" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
